e5c13d2a94751bca82bdb6dedea89ae8559e5662
Subham Sharma <ssuvam670@gmail.com>
2023-12-08 13:07:58 +0545

    Modified themecolor

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-const Color themeColor = Color.fromRGBO(147, 149, 211, 1);
+const Color themeColor = Color.fromRGBO(106, 99, 246, 1);
 const Color bgColor = Color.fromRGBO(214, 215, 239, 1);
-const iconColor=Color.fromRGBO(179, 183, 238, 1);
+const iconColor=Color.fromRGBO(109, 103, 224, 1);
